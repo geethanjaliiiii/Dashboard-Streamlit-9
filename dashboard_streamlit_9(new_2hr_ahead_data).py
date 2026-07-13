893c80bb098569d211428ba4cd09d8d9b9affe4a
geethanjaliiiii <geethanjali.tk@spatialty.ai>
2026-07-13 11:20:23 +0530

    Update dashboard_streamlit_9(new_2hr_ahead_data).py

--- dashboard_streamlit_9(new_2hr_ahead_data).py
+++ dashboard_streamlit_9(new_2hr_ahead_data).py
@@ -364,21 +364,14 @@ else:
     else:
         pass
 
-    fig1.update_layout(
-        title=dict(
-            text=f"Forecasted GHI for {selected_date}",
-            font=dict(size=24)
-        ),
-        xaxis_title="Time",
-        yaxis_title="GHI",
-        height=450,
-        legend=dict(
-            orientation="h",
-            yanchor="bottom",
-            y=1.02,
-            xanchor="right",
-            x=1
-        )
+    show_section_heading(
+        title="Forecasted GHI",
+        start_date=selected_date,
+        end_date="",
+        icon="☀️",
+        heading_size="1.55rem",
+        top_margin="0px",
+        bottom_margin="10px"
     )
 
     fig1.update_xaxes(
